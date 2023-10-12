fb3dac9b14ba4b3470de08d06bfb9613b78f7a6a
Johnbhabze <115083981+Johnbhabze@users.noreply.github.com>
2023-10-12 20:28:05 +0800

    Update pims_db.sql

--- pims_db.sql
+++ pims_db.sql
@@ -3,12 +3,10 @@ CREATE TABLE product_info (
     pName VARCHAR(63) NOT NULL,
     pPrice FLOAT(7) NOT NULL,
     pCode VARCHAR(63) NOT NULL, --Unique ID of the product
-    pPromo VARCHAR(63),
+    pPromo VARCHAR(63), --Can be empty
     pQty INT(7) NOT NULL,
-    pCategory ENUM('','','',''),
-    pBrand ENUM('','','',''),
-    pUnit ENUM('g','mg','kg','lb','oz','mL','L'),
-    pUnit VARCHAR(63),
+    pCategory ENUM('Chicken Inasal','Halo - Halo','Palabok','Pork BBQ','Liempo','Sisig','Empanada','Group Sizes'),
+    pSize VARCHAR(7) NOT NULL,
     pWeight FLOAT(7) NOT NULL,
     pDetails TEXT
 );
